43e448162dda8394130a5e819fe75e5f53926f07
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-04-23 04:38:10 +0000

    halfway done with assignment 6
    update needs to be made and delete verified

--- cs3424/assign6.c
+++ cs3424/assign6.c
@@ -2,8 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct {
-  int itemNum;
+#include "assign6.h"
+
+/*#define itemSize sizeof(Item)*/
+
+typedef struct Item_struct {
   char itemName[64];
   char simpleName[16];
   int currentQuantity;
@@ -13,45 +16,53 @@ typedef struct {
 
 void printMenu();
 Item * createItem();
-void printItem(Item * item);
+void printItem(Item *);
+void readFile(FILE *);
+void delete(FILE *);
+void update(FILE *);
+void create(FILE *);
+void getString(const char *, char [], int);
+long byteOffset();
+int itemExists(long, FILE *);
 
 char *inventoryFile = "inventory.dat";
 
-int main(void) {
+int main(int argc, char *argv[]) {
   char input[7];
   memset(input,0,sizeof(input));
   printMenu();
 
   FILE *fp;
   if(!(fp = fopen(inventoryFile, "r"))){
-    ip = fopen(inventoryFile, "w");
+    fp = fopen(inventoryFile, "w");
     if(!fp){
       printf("Error creating file\n");
       return 1;
     }
   }
-  
-  while(fgets(input,strlen(input),stdin) != NULL){
+  while(fgets(input,sizeof(input),stdin) != NULL){
+    input[strcspn(input, "\n")] = '\0';
     
     switch(input[0]) {
       case 'c':
       case 'C':
         printf("Create new item\n");
-        Item *item = createItem();
-        printItem(item);
+        create(fp);
         break;
       case 'r':
       case 'R':
         printf("Read item\n");
-        read(fp);
+        readFile(fp);
         break;
       case 'u':
-      case 'U'
+      case 'U':
         printf("Update item\n");
+        update(fp);
         break;
       case 'd':
       case 'D':
         printf("Delete item\n");
+        delete(fp);
         break;
       case 'q':
       case 'Q':
@@ -66,9 +77,8 @@ int main(void) {
     }
     printMenu();
   }
-
   
-  close(fp);
+  fclose(fp);
   return 0;
 }
 
@@ -78,45 +88,49 @@ void printMenu() {
 
 void printItem(Item *item) {
   printf("\nPRINTING ITEM\n");
-  printf("Item Number: %d\n", item->itemNum);
   printf("Simple name: %s\n", item->simpleName);
   printf("Item name: %s\n", item->itemName);
   printf("Current/Max quantity: %d/%d\n", item->currentQuantity, item->maxQuantity);
   printf("Body: %s\n", item->body);
 }
 
-void read(FILE *fp) {
+void readFile(FILE *fp) {
   int byte = byteOffset();
   if (byte == -1) {
     return;
   }
   
   fp = fopen("inventory.dat", "r");
-  Item temp;
+  Item *temp = malloc(sizeof(Item));
   fseek(fp, byte, SEEK_SET);
-  fread(&temp, size(Item), 1, fp);
-  if(temp.maxQuantity == 0){
+  fread(temp, sizeof(Item), 1, fp);
+  if(temp->maxQuantity == 0 || !itemExists(byte,fp)){
     printf("Item not valid / found.\n");
     return;
   }
-  printItem(&temp);
+  printItem(temp);
 }
 
-void delete(File *fp) {
+void delete(FILE *fp) {
   int byte = byteOffset();
   if(byte == -1) {
     return;
   }
   fp = fopen(inventoryFile, "r+b");
 
-  itemExists(byte, fp);
-  for(int i = 0; i < size(Item); i++)) {
-    //fseek(fp, byte + i, SEEK_SET);
-    //fwrite(&temp, size(Item), 1, fp);
+  if(!itemExists(byte, fp)){
+      printf("Item not found!\n");
+    return;
   }
+  int itemSize = sizeof(Item);
+  for(int i = 0; i < itemSize; i++) {
+    fseek(fp, byte + i, SEEK_SET);
+    fputc(0, fp);
+  }
+  fflush(fp);
 }
 
-void update(File *fp) {
+void update(FILE *fp) {
   int byte = byteOffset();
   if (byte == -1) {
     return;
@@ -137,31 +151,60 @@ void create(FILE *fp) {
   if (byte == -1) {
     return;
   }
+  
   fp = fopen(inventoryFile, "r+b");
   if (itemExists(byte, fp)){
     printf("Item already exists.\n");
+    return;
   }
-  Item temp;
-  //enter things with getstring
+  
+  Item *temp = malloc(sizeof(Item));
+  memset(temp, 0, sizeof(Item));
+
+  char buffer[128];
+
+  printf("Enter simple name: ");
+  myReader(buffer);
+  strncpy(temp->simpleName, buffer, sizeof(temp->simpleName));
+
+  printf("Enter item name: ");
+  myReader(buffer);
+  strncpy(temp->itemName, buffer, sizeof(temp->itemName));
+  
+  printf("Enter current quantity: ");
+  myReader(buffer);
+  temp->currentQuantity = ((buffer[0]=='0')) ? 0 : atoi(buffer);
+  
+  printf("Enter max quantity: ");
+  myReader(buffer);
+  temp->maxQuantity = ((buffer[0]=='0'))  ? 0 : atoi(buffer);
+
+  printf("Enter description: ");
+  myReader(buffer);
+  strncpy(temp->body, buffer, sizeof(temp->body));
+
+  printItem(temp);
+  
   fseek(fp, byte, SEEK_SET);
-  fwrite(temp, size(Item), 1, fp);
+  fwrite(temp, sizeof(Item), 1, fp);
   fflush(fp);
+  printf("\n");
 }
 
-void getString(const char *message, char variable[], int something) {
+/*void getString(const char *message, char variable[], int something) {
   char buffer[128];
   memset(buffer, 0, sizeof(buffer));
-}
+}*/
 
-int itemExists(int byte, File *fp) {
+int itemExists(long byte, FILE *fp) {
   Item validate;
   fseek(fp, byte, SEEK_SET);
 
-  if(fread(&validate, size(Item), 1, fp) != 1 || validate.maxQuantity == 0){
+  if(fread(&validate, sizeof(Item), 1, fp) != 1 || validate.maxQuantity == 0){
     return 0;
   }  
 
-  
+  return 1;
 }
 
 long byteOffset() {
@@ -173,112 +216,11 @@ long byteOffset() {
   printf("Enter item number: ");
   fgets(num, sizeof(num), stdin);
   num[strcspn(num, "\n")] = '\0';
-
   retNum = strtol(num, &numPtr, 10);
-  if(retNum < 1 || retNum > 9999)) {
+  
+  if(retNum < 1 || retNum > 9999) {
     printf("Invalid item number\n");
     return -1;
   }
-  return size(Item) * retNum;
+  return sizeof(Item) * retNum;
 }
-
-Item * createItem() {
-  /*
-  printf("CREATING ITEM\n");
-  //char buffer[128];
-  //memset(buffer, 0, sizeof(buffer));
-  Item *item = malloc(sizeof(Item));
-  //fflush(stdin);
-  
-  //TODO: THIS SHOULDNT BE A STRUCT FEATURE
-  //printf("Enter item number:");
-  //fgets(buffer, 100 , stdin);
-  int temp;
-  //sscanf(buffer, "%d", &temp);
-  scanf("%d", &temp);
-  item->itemNum = temp;
-  //printf("%d",temp);
-
-  //printf("Enter simple name:");
-  //fgets(buffer, 16, stdin);
-  //strncpy(item->simpleName, buffer, 16);
-  //sscanf(buffer, "%s", item->simpleName);
-  //scanf("%s", item->simpleName);
-  fgets(item->simpleName, strlen(item->simpleName), stdin);
-  item->simpleName[strlen(item->simpleName) - 1] = '\0';
-  
-  
-  //printf("Enter item name:");
-  //fgets(buffer, 64, stdin);
-  //sscanf(buffer, "%s", item->itemName);
-  //scanf("%s", item->itemName);
-  fgets(item->itemName, strlen(item->itemName), stdin);
-  item->itemName[strlen(item->itemName) - 1] = '\0';
-  
-  //printf("Enter current quantity:");
-  //fgets(buffer, 10 , stdin);
-  //sscanf(buffer, "%d", &item->currentQuantity);
-  scanf("%d", &item->currentQuantity);
-  
-  //printf("Enter max quantity:");
-  //fgets(buffer, 10 , stdin);
-  //sscanf(buffer, "%d", &item->maxQuantity);
-  scanf("%d", &item->maxQuantity);
-  
-  //printf("Enter body:");
-  //fgets(buffer, 128 , stdin);
-  //sscanf(buffer, "%s", item->body);
-  //scanf("%s", item->body);
-  fgets(item->body, strlen(item->body), stdin);
-  
-  return item;
-  */
-  printf("CREATING ITEM\n");
-  Item *item = malloc(sizeof(Item));
-
-  // Read item number
-  printf("Enter item number: ");
-  scanf("%d", &item->itemNum);
-
-  // Read item name (using fgets for safety)
-  printf("Enter item name: ");
-  fgets(item->itemName, sizeof(item->itemName), stdin);
-  // Remove trailing newline if present
-  item->itemName[strcspn(item->itemName, "\n")] = '\0';
-
-  // Read simple name (using scanset specifier)
-  printf("Enter simple name: ");
-  scanf("%15[^ \n]%*c", item->simpleName); // Read max 15 chars excluding space/newline, discard newline
-
-  // Read quantities
-  printf("Enter current quantity: ");
-  scanf("%d", &item->currentQuantity);
-  printf("Enter max quantity: ");
-  scanf("%d", &item->maxQuantity);
-
-  // Read body (using fgets for safety)
-  printf("Enter item body:\n"); // Separate prompt for multi-line input
-  fgets(item->body, sizeof(item->body), stdin);
-  // Remove trailing newline if present
-  item->body[strcspn(item->body, "\n")] = '\0';
-
-  return item;
-}
-
-/*Item createItem2() {
-  printf("CREATING ITEM 2\n");
-  Item item;
-  printf("Enter item number: ");
-  scanf("%d", &item.itemNum);
-  printf("Enter simple name: ");
-  scanf("%s", item.simpleName);
-  printf("Enter item name: ");
-  scanf("%s", item.itemName);
-  printf("Enter current quantity: ");
-  scanf("%d", &item.currentQuantity);
-  printf("Enter max quantity: ");
-  scanf("%d", &item.maxQuantity);
-  printf("Enter body: ");
-  scanf("%s", item.body);
-  return item;
-}*/
--- cs3424/assign6.h
+++ cs3424/assign6.h
@@ -1,7 +1,9 @@
 #ifndef ASSIGN6_H
 #define ASSIGN6_H
 
-
+#define myReader(buffer)  memset(buffer,0,sizeof(buffer));\
+                          fgets(buffer,sizeof(buffer),stdin);\
+                          buffer[strcspn(buffer, "\n")] = '\0'
 
 
 
